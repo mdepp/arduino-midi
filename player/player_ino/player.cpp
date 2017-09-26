f3473758253ede6631cd0d219469187c05ab2b49
mdepp <mdepp@ualberta.ca>
2017-09-25 21:24:38 -0600

    Updated player to play from circular buffer

--- player/player_ino/player.cpp
+++ player/player_ino/player.cpp
@@ -1,33 +1,76 @@
-#include <Arduino.h>
-
 /*
- * Simple test of port manipulation and digital-to-analogue conversion.
+ * Plays a sound file from memory
  * 
  * Arduino ports 22 through 29 are connected to an R-2R resistor ladder for DAC,
  * (pin 29 is most-significant) the output of which connects to the speaker.
  */
 
+#include <Arduino.h>
+#include "timer.h"
+
+
+const uint32_t CLOCK_SPEED = 16000000;
+
+// Sound buffer
+const uint32_t BUFFER_SIZE = 5000;
+byte buffer[BUFFER_SIZE];
+const uint32_t SAMPLE_RATE = 8000;
+uint32_t current_position, end_position;
+
+// Speaker
+uint8_t* output_filter_register = &DDRA;
+uint8_t* output_register = &PORTA;
+
+void initSoundBuffer()
+{
+  // Init with 440hz square wave
+  byte state = 0;
+  for (uint32_t i=0; i<BUFFER_SIZE; ++i)
+  {
+    if (SAMPLE_RATE/440 > 0)
+    {
+      if (i % (SAMPLE_RATE/440) == 0)
+      {
+        state ^= B11111111;
+      }
+    }
+    else
+    {
+      state ^= B11111111;
+    }
+    buffer[i] = state;
+  }
+  current_position = 0;
+  end_position = BUFFER_SIZE - 1;
+}
+
+void initSpeaker()
+{
+  *output_filter_register = B11111111;
+}
+
 int main()
 {
-  DDRA = B11111111; // Initialize for output
+  Serial.begin(9600);
+  initSoundBuffer();
+  initSpeaker();
+  // Initialize timer 1
+  uint32_t actual_sample_rate = Timer3::initTimer(CLOCK_SPEED, SAMPLE_RATE, &tickSound);
+  Serial.print("Actual sample rate: "); Serial.println(actual_sample_rate);
+  
+  while (true) {};
 
-  uint32_t NOTE_MAX = 128;
-  uint32_t freq_hz = 440;
-  uint32_t period_us = 1000000 / freq_hz;
-  uint32_t delay_time_us = period_us / NOTE_MAX / 2;
+  Timer3::quitTimer();
 
-  while (true) // Output a triangular wave
-  {
-    for (int i=0; i<NOTE_MAX; ++i)
-    {
-        PORTA = i;
-        delayMicroseconds(delay_time_us);
-    }
-    for (int i=NOTE_MAX-1; i>=0; --i)
-    {
-        PORTA = i;
-        delayMicroseconds(delay_time_us);
-    }
-  }
   return 0;
+}
+
+
+void tickSound()
+{
+  // Circle to start of buffer upon overflow
+  if (current_position == end_position)
+    current_position = 0;
+  // Update speaker to next part of buffer
+  *output_register = buffer[++current_position];
 }
---
+++ player/player_ino/timer.h
@@ -0,0 +1,98 @@
+#ifndef _TIMER_H
+#define _TIMER_H
+
+namespace Timer3
+{
+    typedef void (*Callback) ();
+    Callback callback;
+
+    
+    // Called with frequency as set by initTimer. Assumes callback is set.
+    ISR(TIMER3_COMPA_vect)
+    {
+        callback();
+    }
+    
+    struct ScalingData
+    {
+        uint16_t scaling;
+        uint32_t compare_match;
+        uint32_t real_frequency;
+    };
+    ScalingData calculateScaling(uint32_t clock_speed, uint32_t ideal_frequency)
+    {
+        uint8_t num_options = 5;
+        uint16_t options[] = {1024, 256, 64, 8, 1};
+
+        uint32_t smallest_difference;
+        int8_t best_index = -1;
+
+        for (int8_t i=0; i<num_options; ++i)
+        {
+            uint32_t compare_match_high = clock_speed/options[i]/ideal_frequency; //1+compare match
+            uint32_t real_frequency = clock_speed/options[i]/compare_match_high;
+            uint32_t difference = abs(ideal_frequency-real_frequency);
+
+            if (best_index==-1 || difference < smallest_difference)
+            {
+                best_index = i;
+                smallest_difference = difference;
+            }
+        }
+
+        ScalingData re;
+        re.scaling = options[best_index];
+        re.compare_match = clock_speed/re.scaling/ideal_frequency-1;
+        re.real_frequency = clock_speed/re.scaling/(re.compare_match+1);
+        return re;
+    }
+    uint8_t getScalingFlags(uint16_t scaling)
+    {
+        switch (scaling)
+        {
+            case 1: return (1<<CS30);
+            case 8: return (1<<CS31);
+            case 64: return (1<<CS31) | (1<<CS30);
+            case 246: return (1<<CS32);
+            case 1024: return (1<<CS32) | (1<<CS30);
+            default: return (1<<CS30); // clk/1 for invalid options
+        }
+    }
+
+    /*
+     * Initialize timer1. Attempts to match clock1 settings with specified frequency,
+     * and will return the actual frequency that the callback is called.
+     */
+    uint32_t initTimer(uint32_t clock_speed, uint32_t ideal_frequency, Callback cb)
+    {
+        callback = cb;
+        // Initialize timer1
+        cli(); // Disable global interrupts
+        TCCR1A = 0;
+        TCCR1B = 0;
+        // Calculate scaling
+        ScalingData sd = calculateScaling(clock_speed, ideal_frequency);
+
+        TCCR3B |= (1 << WGM32); // Turn on CTC mode
+        TCCR3B |= getScalingFlags(sd.scaling); // Turn on appropriate scaling
+        TIMSK3 |= (1 << OCIE3A); // Enable compare interrupt
+        
+        OCR3A = sd.compare_match; // Set compare match register to desired timer count
+
+        sei(); // Enable global interrupts
+
+        return sd.real_frequency;
+    }
+
+    // Stop timer1
+    void quitTimer()
+    {
+        // Clear CS10, 1, 2
+        TCCR3B &= ~(1 << CS30);
+        TCCR3B &= ~(1 << CS31);
+        TCCR3B &= ~(1 << CS32);
+    }
+};
+
+
+#endif // _TIMER_H
