fcb5a3b4eae60ca47e4db97c1a01796d53cdb2cc
mdepp <mdepp@ualberta.ca>
2017-09-28 20:13:19 -0600

    Cleaned up music player code

--- player/player_ino/player.cpp
+++ player/player_ino/player.cpp
@@ -9,62 +9,78 @@
 #include "timer.h"
 
 
+// Timing information
 const uint32_t CLOCK_SPEED = 16000000;
+const uint32_t SAMPLE_RATE = 8000;
+uint32_t BAUD_RATE = 128000;
 
-// Sound buffer
+// Signal sent to request another buffer of sound data from serial input
+const byte BUFFER_SIGNAL = 1;
+
+// Controls which pins are used for the digital sound output
+volatile uint8_t* OUTPUT_FILTER_REGISTER = &DDRA;
+volatile uint8_t* OUTPUT_REGISTER = &PORTA;
+
+// Sound buffers
 const uint32_t BUFFER_SIZE = 1000;
 byte rb[BUFFER_SIZE];
 byte wb[BUFFER_SIZE]; // Todo: volatile necessary?
-byte* read_buffer = rb;
-byte* write_buffer = wb;
+byte* read_buffer = wb; // For easy swapping
+byte* write_buffer = rb;
 
-const uint32_t SAMPLE_RATE = 8000; //440; //8000;
+ // A flag to notify main process when buffers have been swapped by interrupt
+volatile bool buffersSwapped;
 
-volatile uint32_t read_position;
-uint32_t write_position;
 
-volatile bool buffersSwapped; // Triggered by 'read' event when it swaps read/write buffers
-
-uint32_t BAUD_RATE = 128000;
-
-// Speaker
-volatile uint8_t* output_filter_register = &DDRA;
-volatile uint8_t* output_register = &PORTA;
-
-void initSoundBuffers()
+/*
+ * Fill the write buffer with the next batch of information through the serial port.
+ * This will delay until the requested data is received.
+ */
+void fillWriteBuffer()
 {
-  for (uint32_t i=0; i<BUFFER_SIZE; ++i)
+  // Signal for more data
+  Serial.write(BUFFER_SIGNAL); Serial.flush();
+  // Read the data one byte at a time
+  uint32_t write_position = 0;
+  while (write_position < BUFFER_SIZE)
   {
-    read_buffer[i] = write_buffer[i] = 0;
+    if (Serial.available() > 0)
+    {
+      write_buffer[write_position++] = Serial.read();
+    }
   }
-  write_position = 0;
-  read_position = 0;
-  buffersSwapped = false;
 }
-
-void initSpeaker()
+/*
+ * Swap the write and read buffers, so that the new read buffer can be read
+ * while the data from the old one is overwritten. Because this operation is not
+ * atomic, this function should *not* be called if it could be interrupted by
+ * the buffer reading operation.
+ */
+void swapBuffers()
 {
-  *output_filter_register = B11111111;
+  byte* temp = read_buffer;
+  read_buffer = write_buffer;
+  write_buffer = temp;
+
 }
 
-void swap(byte** first, byte** second)
-{
-  byte* temp = *first;
-  *first = *second;
-  *second = temp;
-}
-
+/*
+ * Reads one byte from the read buffer and outputs it to the output register.
+ * If this is the last byte in that buffer, it will swap buffers and notify the
+ * main process by setting the 'buffersSwapped' flag.
+ */
 void tickSound()
 {
+  static volatile uint32_t read_position = 0;
+
   // Update speaker to next part of buffer
-  *output_register = read_buffer[read_position];
+  *OUTPUT_REGISTER = read_buffer[read_position];
 
   // Swap buffers upon overflow
-  ++read_position;
-  if (read_position >= BUFFER_SIZE)
+  if (++read_position >= BUFFER_SIZE)
   {
-    swap(&read_buffer, &write_buffer);
-    buffersSwapped = true;
+    swapBuffers();
+    buffersSwapped = true; // Let the main process know that buffers were swapped
     read_position = 0;
   }
 }
@@ -73,24 +89,13 @@ int main()
 {
   init();
   Serial.begin(BAUD_RATE);
-  initSoundBuffers();
-  initSpeaker();
+  *OUTPUT_FILTER_REGISTER = B11111111;
 
-  // Fill write buffer
-  Serial.write(1); Serial.flush();
-  write_position = 0;
-  while (write_position < BUFFER_SIZE)
-  {
-    if (Serial.available() > 0)
-    {
-      write_buffer[write_position] = Serial.read();
-      ++write_position;
-    }
-  }
+  // Fill write buffer before starting any reading
+  fillWriteBuffer();
   // Swap buffers, giving read buffer some initial data
-  write_position = 0;
-  swap(&write_buffer, &read_buffer);
-  buffersSwapped = true;
+  swapBuffers();
+  buffersSwapped = true;  // Immediately start writing to the new write buffer
 
   // Start reading sound buffer
   Timer3::initTimer(CLOCK_SPEED, SAMPLE_RATE, tickSound);
@@ -98,19 +103,10 @@ int main()
   // Continue reading while write buffer loads next data.
   while (true)
   {
-    if (buffersSwapped) // Raad has overflowed read buffer and swapped buffers
+    if (buffersSwapped) // Buffers have been swapped so write buffer is ready
     {
-      // So write new data to the new write buffer
-      Serial.write(1); // Request another buffer of data
-      write_position = 0;
-      while (write_position < BUFFER_SIZE)
-      {
-        if (Serial.available() > 0)
-        {
-          write_buffer[write_position] = Serial.read();
-          ++write_position;
-        }
-      }
+      // Overwrite write buffer with new sound data
+      fillWriteBuffer();
       buffersSwapped = false;
     }
   }
